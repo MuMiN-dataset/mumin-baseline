9a10406f74523892ae6edc0482cf6f8ca21465a0
saattrupdan <saattrupdan@gmail.com>
2021-11-24 14:52:48 +0000

    feat: Simplify loading of dataset

--- src/data.py
+++ src/data.py
@@ -16,9 +16,6 @@ def load_mumin_graph(size: str = 'small') -> dgl.DGLHeteroGraph:
             The size of the dataset to compile. Can be 'small', 'medium' and
             'large'.
     '''
-    dataset = MuminDataset(os.getenv('TWITTER_API_KEY'),
-                           size=size, include_articles=False,
-                           include_replies=False)
+    dataset = MuminDataset(os.getenv('TWITTER_API_KEY'), size=size)
     dataset.compile()
-    #dataset.add_embeddings(nodes_to_embed=['user'])
     return dataset.to_dgl()
