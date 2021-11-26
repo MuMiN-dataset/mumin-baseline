9a6e507a2781bacdbd4809a0664564a1d9500f08
saattrupdan <saattrupdan@gmail.com>
2021-11-26 16:53:57 +0000

    feat: Fix label bug, set up training with validation set

--- src/train.py
+++ src/train.py
@@ -7,8 +7,19 @@ from pathlib import Path
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import torchmetrics as tm
 from dgl.data.utils import save_graphs, load_graphs
+import dgl
+import logging
+from termcolor import colored
+
+
+# Set up logging
+fmt = (colored('%(asctime)s [%(levelname)s] <%(name)s>\n↳ ', 'green') +
+       colored('%(message)s', 'yellow'))
+logging.basicConfig(level=logging.INFO, format=fmt)
+logger = logging.getLogger(__name__)
 
 
 def train(num_epochs: int,
@@ -29,6 +40,10 @@ def train(num_epochs: int,
             corresponding to doing thread-level or claim-level node
             classification. Defaults to 'claim'.
     '''
+    # Set random seeds
+    torch.manual_seed(4242)
+    dgl.seed(4242)
+
     # Set up PyTorch device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -52,12 +67,12 @@ def train(num_epochs: int,
 
     # Store labels and masks
     labels = graph.nodes[task].data['label']
-    train_mask = graph.nodes[task].data['train_mask'].long()
-    val_mask = graph.nodes[task].data['val_mask'].long()
+    train_mask = graph.nodes[task].data['train_mask']
+    val_mask = graph.nodes[task].data['val_mask']
 
     # Initialise dictionary with feature dimensions
-    dims = dict(claim=870, user=774, tweet=811, reply=824, image=768,
-                article=1536, hashtag=1)
+    dims = {ntype: graph.nodes[ntype].data['feat'].shape[-1]
+            for ntype in graph.ntypes}
     feat_dict = {rel: (dims[rel[0]], hidden_dim, dims[rel[2]])
                  for rel in graph.canonical_etypes}
 
@@ -66,13 +81,24 @@ def train(num_epochs: int,
     model.to(device)
     model.train()
 
+    # Set up path to state dict, and load model weights if they exist
+    model_path = Path(f'{task}-model-{size}-{hidden_dim}.pt')
+    if model_path.exists():
+        model.load_state_dict(torch.load(str(model_path)))
+
     # Initialise optimiser
-    opt = optim.Adam(model.parameters(), lr=2e-5)
+    opt = optim.AdamW(model.parameters(), lr=5e-5, betas=(0.8, 0.998))
+
+    # Initialise learning rate scheduler
+    scheduler = ReduceLROnPlateau(optimizer=opt, factor=0.8, patience=10)
 
     # Initialise scorer
     scorer = tm.F1(num_classes=2, average='none')
 
     for epoch in range(num_epochs):
+
+        # Reset the gradients
+        opt.zero_grad()
 
         # Forward propagation
         logits = model(graph, feats)
@@ -93,33 +119,45 @@ def train(num_epochs: int,
             )
 
             # Compute training metrics
-            train_scores = scorer(logits[train_mask].ge(0), labels[train_mask])
+            train_scores = scorer(logits[train_mask].ge(0), labels)
             train_misinformation_f1 = train_scores[0]
             train_factual_f1 = train_scores[1]
 
             # Compute validation metrics
-            val_scores = scorer(logits[val_mask].ge(0), labels[val_mask])
+            val_scores = scorer(logits[val_mask].ge(0), labels)
             val_misinformation_f1 = val_scores[0]
             val_factual_f1 = val_scores[1]
 
         # Backward propagation
-        opt.zero_grad()
         loss.backward()
+
+        # Update gradients
         opt.step()
 
-        # Report loss and score
-        print(f'Epoch {epoch}')
-        print('> Train loss:', loss.item())
-        print('> Train misinformation F1:', train_misinformation_f1.item())
-        print('> Train factual F1:', train_factual_f1.item())
-        print('> Val loss:', val_loss.item())
-        print('> Val misinformation F1:', val_misinformation_f1.item())
-        print('> Val factual F1:', val_factual_f1.item())
-        print()
+        # Update learning rate
+        scheduler.step(val_loss)
+
+        # Gather statistics to be logged
+        stats = [
+            ('Train loss', loss.item()),
+            ('Train misinformation F1', train_misinformation_f1.item()),
+            ('Train factual F1', train_factual_f1.item()),
+            ('Validation loss', val_loss.item()),
+            ('Validation misinformation F1', val_misinformation_f1.item()),
+            ('Validation factual F1', val_factual_f1.item()),
+            ('Learning rate', opt.param_groups[0]['lr'])
+        ]
+
+        # Report statistics
+        log = f'Epoch {epoch}\n'
+        for statistic, value in stats:
+            log += f'> {statistic}: {value}\n'
+        logger.info(log)
 
         # Save model
-        # TODO
+        torch.save(model.state_dict(), str(model_path))
 
 
 if __name__ == '__main__':
-    train(num_epochs=1000, hidden_dim=100, task='claim')
+    # Train model
+    train(num_epochs=1000, hidden_dim=500, task='claim')
