c1e723f2f61e1cd3809572aa8d993b2889c992d1
saattrupdan <saattrupdan@gmail.com>
2021-12-01 16:37:03 +0000

    feat: Add class weights to text model

--- src/train_text_model.py
+++ src/train_text_model.py
@@ -3,9 +3,49 @@
 from transformers import (AutoTokenizer, AutoModelForSequenceClassification,
                           AutoConfig, TrainingArguments, Trainer)
 from datasets import Dataset, load_metric
-from typing import Dict
+from typing import Dict, List
+import torch.nn as nn
+import torch
 import sys
 import pandas as pd
+
+
+class TrainerWithClassWeights(Trainer):
+    def __init__(self, class_weights: List[float], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.class_weights = class_weights
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        '''Compute the loss for a batch of inputs.
+
+        Args:
+            model (PyTorch model):
+                The model to train.
+            inputs (PyTorch tensor):
+                Tuple containing the inputs.
+            return_outputs (bool, optional):
+                Whether or not to return the outputs. Defaults to False.
+
+        Returns:
+            tuple or float:
+                If return_outputs is True, returns a tuple containing the loss
+                and the outputs. Otherwise, returns the loss.
+        '''
+        # Fetch the labels
+        labels = inputs.get("labels")
+
+        # Forward pass
+        outputs = model(**inputs)
+        logits = outputs.get('logits')
+
+        # Reshape the logits
+        logits = logits.view(-1, self.model.config.num_labels)
+
+        # Compute custom loss
+        loss_fct = nn.CrossEntropyLoss(weight=torch.tensor(self.class_weights))
+        loss = loss_fct(logits, labels.view(-1))
+
+        return (loss, outputs) if return_outputs else loss
 
 
 def main(model_id: str) -> Dict[str, float]:
@@ -86,12 +126,13 @@ def main(model_id: str) -> Dict[str, float]:
     )
 
     # Initialise the Trainer
-    trainer = Trainer(model=model,
-                      args=training_args,
-                      train_dataset=train,
-                      eval_dataset=val,
-                      tokenizer=tokenizer,
-                      compute_metrics=compute_metrics)
+    trainer = TrainerWithClassWeights(model=model,
+                                      args=training_args,
+                                      train_dataset=train,
+                                      eval_dataset=val,
+                                      tokenizer=tokenizer,
+                                      compute_metrics=compute_metrics,
+                                      class_weights=[1., 20.])
 
     # Train the model
     trainer.train()
