dbd75c1baf4f441cca5c5c9678ba8fb72a2e0ab9
saattrupdan <saattrupdan@gmail.com>
2021-12-17 18:33:45 +0000

    feat: Custom apply_node_func

--- src/model.py
+++ src/model.py
@@ -53,30 +53,44 @@ class SAGEConv(nn.Module):
                  in_feats: int,
                  out_feats: int,
                  dropout: float,
+                 hidden_feats: int = 32,
                  activation: Optional[Callable] = None):
         super().__init__()
         self._in_src_feats, self._in_dst_feats = expand_as_pair(in_feats)
         self._out_feats = out_feats
+        self.proj_src = nn.Linear(self._in_src_feats, hidden_feats)
         self.proj_dst = nn.Linear(self._in_dst_feats, out_feats)
         self.fc = nn.Linear(self._in_src_feats + self._in_dst_feats, out_feats)
         self.dropout = nn.Dropout(dropout)
         self.activation = (lambda x: x) if activation is None else activation
 
-    @staticmethod
-    def _message(edges):
-        breakpoint()
-        return {'m': edges.src['h']}
+    def _message(self, edges):
+        src_feats = edges.src['h']
+        src_feats = F.layer_norm(src_feats, normalized_shape=src_feats.shape)
+        src_feats = self.proj_src(src_feats)
+        src_feats = F.gelu(src_feats)
+        return {'m': src_feats}
 
-    @staticmethod
-    def _reduce(nodes):
+    def _reduce(self, nodes):
         messages = nodes.mailbox['m']
-        return {'neigh': messages.max(dim=1)[0]}
+        return {'neigh': messages.mean(dim=1)[0]}
+
+    def _apply_node(self, nodes):
+        breakpoint()
+        h_src = nodes.data['h']
+        h_neigh = nodes.data['neigh']
+        h = torch.cat((h_src, h_neigh), dim=-1)
+        h = self.dropout(h)
+        rst = self.fc(h)
+        return self.proj_dst(h_src) + self.activation(rst)
 
     def forward(self, graph, feat):
         h_src, h_dst = expand_as_pair(feat)
 
         graph.srcdata['h'] = h_src
-        graph.update_all(message_func=self._message, reduce_func=self._reduce)
+        graph.update_all(message_func=self._message,
+                         reduce_func=self._reduce,
+                         apply_node_func=self._apply_node)
         h_neigh = graph.dstdata['neigh']
 
         h = torch.cat((h_dst, h_neigh), dim=-1)
