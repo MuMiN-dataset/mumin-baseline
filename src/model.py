abec34a916c954bd77886bc3129e822b5aeb7e45
saattrupdan <saattrupdan@gmail.com>
2021-12-19 23:24:07 +0000

    feat: Breakpoint

--- src/model.py
+++ src/model.py
@@ -87,9 +87,9 @@ class SAGEConv(nn.Module):
     def _apply_node(self, nodes):
         h_dst = nodes.data['h']
         h_neigh = nodes.data['neigh']
+        breakpoint()
         h = torch.cat((h_dst, h_neigh), dim=-1)
         h = self.dropout(h)
-        print(h.shape, self._in_src_feats, self._in_dst_feats)
         h = self.fc(h)
         h = self.activation(h)
         h = self.norm_concat(h)
